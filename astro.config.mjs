dca9b5000cb54d3638a88e2326db78018e9f1b79
Gabriel Rauta Zanette <85682369+MacawBrasil@users.noreply.github.com>
2023-02-14 09:19:05 -0300

    remove svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
   output: "server",
   adapter: netlify(),
 });
